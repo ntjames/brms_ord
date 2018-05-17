33f41f984301cddc32c143e5348a85469a4e45af
ntjames <nj1154@gmail.com>
2018-05-17 18:26:13 -0500

    modify init_r and priors for non-logit links

--- brms_ord_link_reprex.r
+++ brms_ord_link_reprex.r
@@ -1,4 +1,5 @@
 # reprex
+rm(list=ls())
 library(dplyr)
 library(brms)
 
@@ -17,10 +18,25 @@ fit_logit <- brm(as.numeric(y) ~ z1+z2, data=dat10levs, family=cumulative("logit
                  chains=1, iter=1000, seed=5, refresh=0) 
 
 fit_probit <- brm(as.numeric(y) ~ z1+z2, data=dat10levs, family=cumulative("probit"),
-                  chains=1, iter=1000, seed=5, refresh=0, inits="0")
+                  chains=1, iter=1000, seed=5, refresh=0, init_r=0.001,
+                  prior=set_prior("normal(0,5)",class="b"))
 
-fit_probit_app <- brm(as.numeric(y) ~ z1+z2, data=dat10levs, family=cumulative("probit_approx"),
-                      chains=1, iter=1000, seed=5, refresh=0, inits="0")
+prior_summary(fit_probit)
 
+# don't need probit_approx if probit is working
+#fit_probit_app <- brm(as.numeric(y) ~ z1+z2, data=dat10levs, family=cumulative("probit_approx"),
+#                      chains=1, iter=1000, seed=5, refresh=0)
+
+
+#try manually specifying intercept prior to be more informative
 fit_cloglog <- brm(as.numeric(y) ~ z1+z2, data=dat10levs, family=cumulative("cloglog"),
-                   chains=1, iter=1000, seed=5, refresh=0, inits="0")
+                   chains=1, iter=1000, seed=5, refresh=0, init_r=0.000001,
+                   prior=set_prior("normal(0,5)",class="b"))
+
+fit_cloglog_sratio <- brm(as.numeric(y) ~ z1+z2, data=dat10levs, family=sratio("cloglog"),
+                   chains=1, iter=1000, seed=5, refresh=0)
+
+fitted(fit_cloglog_sratio)
+
+
+get_prior(as.numeric(y) ~ z1+z2, data=dat10levs, family=cumulative("cloglog"))
